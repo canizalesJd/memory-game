81fb893ac7bfc62bb648955bebceccbac7eb5ec1
Jose Canizales <canizalesjd@gmail.com>
2024-04-27 12:59:37 -0600

    Adding vercel env config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV
+  },
   plugins: [react()]
 })
